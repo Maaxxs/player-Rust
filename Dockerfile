832254a78131edd8ebc50048b48091021924fb6c
Max Kunzelmann <maxdev@posteo.de>
2024-10-29 14:52:47 +0100

    more ci stuff

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,8 @@ COPY --from=planner /src/recipe.json /src/recipe.json
 RUN cargo chef cook --release --recipe-path /src/recipe.json
 # Build application
 COPY . .
+ARG GIT_HASH
+ENV GIT_HASH=${GIT_HASH}
 RUN cargo build --release --bin player-Rust
 
 # Use scratch image to reduce image size
--- build.rs
+++ build.rs
@@ -3,7 +3,9 @@ use std::fs::read_to_string;
 fn main() {
     let git_hash_main_branch = ".git/refs/heads/main";
 
-    if let Ok(hash) = std::env::var("GITHUB_SHA") {
+    if let Ok(hash) = std::env::var("GIT_HASH") {
+        println!("cargo:rustc-env=GIT_HASH={}", hash);
+    } else if let Ok(hash) = std::env::var("GITHUB_SHA") {
         println!("cargo:rustc-env=GIT_HASH={}", hash);
     } else if let Ok(hash) = read_to_string(git_hash_main_branch) {
         println!("cargo:rustc-env=GIT_HASH={}", hash);
